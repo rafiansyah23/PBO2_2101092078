6e8462369e8a8713d63c887806d6e92dc899ee1f
rafiansyah23 <114588092+rafiansyah23@users.noreply.github.com>
2023-03-09 15:53:20 +0700

    Create PBO2_2101092078.java

---
+++ PBO2_01092078/src/pbo2_2101092078/PBO2_2101092078.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_2101092078;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import rafi.dao.Koneksi;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PBO2_2101092078 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            Koneksi koneksi = new Koneksi ();
+            Connection con = koneksi.getKoneksi();
+            JOptionPane.showMessageDialog(null, "Koneksi ok");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error: "+ ex.getMessage());
+            Logger.getLogger(PBO2_2101092078.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
+    
+}
