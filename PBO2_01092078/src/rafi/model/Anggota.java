45dc6bfbbb89a9af0db80792d4fadb39ac6281ac
rafiansyah23 <114588092+rafiansyah23@users.noreply.github.com>
2023-03-09 15:53:31 +0700

    Create Anggota.java

---
+++ PBO2_01092078/src/rafi/model/Anggota.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rafi.model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Anggota {
+    private String kodeAnggota;
+    private String namaAnggota;
+    private String alamat;
+    private String jenisKelamin;
+
+    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
+        this.kodeAnggota = kodeAnggota;
+        this.namaAnggota = namaAnggota;
+        this.alamat = alamat;
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+    
+
+    public String getKodeAnggota() {
+        return kodeAnggota;
+    }
+
+    public String getNamaAnggota() {
+        return namaAnggota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setKodeAnggota(String kodeAnggota) {
+        this.kodeAnggota = kodeAnggota;
+    }
+
+    public void setNamaAnggota(String namaAnggota) {
+        this.namaAnggota = namaAnggota;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+    
+    
+}
