aaad46fa20dfa040f1014105a6723f50846f483b
rafiansyah23 <114588092+rafiansyah23@users.noreply.github.com>
2023-07-25 15:16:54 +0700

    Update Koneksi.java

--- PBO2_01092078/src/rafi/dao/Koneksi.java
+++ PBO2_01092078/src/rafi/dao/Koneksi.java
@@ -4,23 +4,25 @@
  * and open the template in the editor.
  */
 package rafi.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 /**
  *
  * @author LENOVO
  */
 public class Koneksi {
-    private String url = "jdbc:mysql://localhost/PBO2_2101092078";
+    private String url = "jdbc:mysql://localhost/pbo2_21010920788";
     private String username = "root";
-    private String password ="";
+    private String password = "";
     
-    public Connection getKoneksi() throws SQLException, ClassNotFoundException{
+    public Connection getKoneksi() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(url, username, password);
     }
-    
+
+   
 }
+
